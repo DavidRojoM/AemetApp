6b17ebfea93368a1cca58e813545546095a4208d
David <davidrojo1997@gmail.com>
2021-04-06 17:51:25 +0200

    First commit

---
+++ app/src/main/java/com/example/aemetprevi_davidrojo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.aemetprevi_davidrojo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //cosas que hacer para inicializar sistemas, audio, fotos... etc.
+    }
+}
