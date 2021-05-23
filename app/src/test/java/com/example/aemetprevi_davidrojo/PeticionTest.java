96bd691b96f3ef2d90c46d38fa52a5ecfeb55bf9
David <davidrojo1997@gmail.com>
2021-05-23 17:34:46 +0200

    pruebas

--- app/src/test/java/com/example/aemetprevi_davidrojo/PeticionTest.java
+++ app/src/test/java/com/example/aemetprevi_davidrojo/PeticionTest.java
@@ -25,6 +25,7 @@ public class PeticionTest {
         assertNull(miPeticion.getUltimaRespuesta());
         miPeticion.getPrevision("https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/29069?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhbmRyZXMuYWxjYXJhekBwb2xpdGVjbmljb21hbGFnYS5jb20iLCJqdGkiOiJjOTc3MjI0OC0xYzNiLTQ4ODAtYjYxYy04YWE5NjNmNGE0YmYiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTYxNzcwNzMyOCwidXNlcklkIjoiYzk3NzIyNDgtMWMzYi00ODgwLWI2MWMtOGFhOTYzZjRhNGJmIiwicm9sZSI6IiJ9.GojMAUMYA_BPK17JnzoxQsVqPgy2y7qQkH9XUGh6qnE",false);
         Thread.sleep(5000);
+        System.out.println(miPeticion.getUltimaRespuesta());
         assertNotNull(miPeticion.getUltimaRespuesta());
 
     }
